a9ce3831b308a6ce82589314fbe22e74cc44bdc2
tarlepp <tarmo.leppanen@kase.fi>
2015-03-14 21:07:54 +0200

    Improved comments, and changed console.log() to Logger.error()

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 'use strict';
 
+/**
+ * index.js
+ *
+ * Main file of the Kärmes AI-bot, this is the first entry point when you start the bot.
+ *
+ * Usage examples:
+ *  npm start [host] [port] [player]
+ *  node index.js [host] [port] [player]
+ *
+ * Where
+ *  host    = Server host, if not given fallback to 'localhost'
+ *  port    = Server port, if not given fallback to '6969'
+ *  player  = Player name, if not given fallback to 'T0P1 (C1-10P)'
+ */
+
+/**#@+
+ * Module dependencies
+ *
+ * @type {exports}
+ */
+/** @type {Connection} */
 var Connection = require('./lib/connection');
 
+/** @type {LoggerInstance} */
+var Logger = require('./lib/logger');
+/**#@-*/
+
 try {
+  Logger.info('Kärmes AI-bot started with following command:', JSON.stringify(process.argv));
+
   // Create new connection
   var connection = new Connection(process.argv[2], process.argv[3], process.argv[4]);
 
   // And connect, this will start actual playing
   connection.connect();
 } catch(error) {
-  console.log('Damn gerbils have stopped running again! Someone has been dispatched to poke them with a sharp stick.');
-  console.log(error);
+  Logger.error('Damn gerbils have stopped running again! Someone has been dispatched to poke them with a sharp stick.');
+  Logger.error(JSON.stringify(error));
 }
