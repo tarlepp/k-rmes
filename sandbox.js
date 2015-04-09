770ec438ab452c13d3d45513a3858861957332eb
tarlepp <tarmo.leppanen@kase.fi>
2015-04-09 18:12:32 +0300

    Renamed file, no more confusing to actual tests.

---
+++ sandbox.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * This file is just for testing some code out-of-the box. Basically content of this file is always changing, depending
+ * which 'problem' I'm struggling currently :D
+ *
+ * Usage:
+ *  node sandbox.js
+ *
+ * And remember to add some actual code.
+ */
+
+/**#@+
+ * Module dependencies
+ *
+ * @type {exports}
+ * @private
+ */
+var _ = require('lodash');
+var Utils = require('./lib/utils');
+var Constants = require('./lib/constants');
+/**#@-*/
+
+console.log('this is just for testing some code...');
